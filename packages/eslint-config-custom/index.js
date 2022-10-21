d40cd47b601086706c8f4064f6b160849148c986
Tony <totran0331@gmail.com>
2022-10-21 16:32:59 -0400

    added prettier and server files

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  "root": true,
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  extends: ["turbo", "prettier", "eslint:recommended",
+  "plugin:@typescript-eslint/eslint-recommended",
+  "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
 };
