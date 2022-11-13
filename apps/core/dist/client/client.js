01fa69d3de44144742f27c1bf66c43a1457ae25a
T <totran0331@gmail.com>
2022-11-13 01:32:18 -0500

    updated gitignore

--- apps/core/dist/client/client.js
+++ apps/core/dist/client/client.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_net_1 = __importDefault(require("node:net"));
+class Client {
+}
 const client = node_net_1.default.createConnection({
     port: 1337
 }, () => {
     client.write('hello\r\n');
 });
-client.on('data', data => {
+client.on('data', (data) => {
     console.log(data.toString());
     client.end();
 });
--- apps/core/src/client/Client.ts
+++ apps/core/src/client/Client.ts
@@ -1,12 +1,17 @@
-import net from 'node:net'
+import net from 'node:net';
 
-const client = net.createConnection({
-	port: 1337
-}, () => {
-	client.write('hello\r\n')
-})
+class Client {}
 
-client.on('data', data => {
-	console.log(data.toString())
-	client.end()
-})
+const client = net.createConnection(
+   {
+      port: 1337
+   },
+   () => {
+      client.write('hello\r\n');
+   }
+);
+
+client.on('data', (data) => {
+   console.log(data.toString());
+   client.end();
+});
